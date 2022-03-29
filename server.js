e052b002c7aa6d7c240752e551480e0d1ac0965d
Enriquelzg <enrique.lazaro@alumnos.upm.es>
2022-03-29 14:22:24 +0200

    Hito 3.2

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const app = require('./app');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-const db = 'mongodb://127.0.0.1:27017/API';
+const db = process.env.MONGODB || 'mongodb://127.0.0.1:27017/API';
 
 // const db = 'mongodb://localhost:27017/API';
 
