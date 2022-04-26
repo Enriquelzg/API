c0928d91427f421c8f462e5f0b36838bbc4c9861
Enriquelzg <enrique.lazaro@alumnos.upm.es>
2022-04-26 14:57:01 +0200

    Pruebas de métodos

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,7 +8,7 @@ exports.getAllusers = function all(req, res) {
 };
 
 exports.getUser = function find(req, res) {
-  User.findById(req.params.id, (err, user) => {
+  User.findById(req.params.userId, (err, user) => {
     if (err) return res.send(500, err.message);
     return res.status(200).jsonp(user);
   });
@@ -23,7 +23,7 @@ exports.adduser = function add(req, res) {
 };
 
 exports.updateuser = function update(req, res) {
-  User.findById(req.params.id, (err) => {
+  User.findById(req.params.userId, (err) => {
     User.name = req.body.name;
     User.password = req.body.password;
     if (err) return res.status(500).send({ err });
@@ -37,7 +37,7 @@ exports.updateuser = function update(req, res) {
 };
 
 exports.deleteuser = function deleteuser(req, res) {
-  User.findById(req.params.id, (user) => {
+  User.findById(req.params.userId, (user) => {
     user.remove((error) => {
       if (error) return req.status(500).send(error.message);
       return res.status(200).send();
